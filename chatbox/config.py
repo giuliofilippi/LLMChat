32b6587731dbbb446e8a5d0200245cfe1691abdd
giuliofilippi <giuliofilippi@me.com>
2023-06-06 11:52:47 +0100

    stylish template

---
+++ chatbox/config.py
@@ -0,0 +1,18 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+# Choose model name from https://huggingface.co/models?filter=flax&pipeline_tag=translation
+model_name_1 = 'google/flan-t5-base'
+model_name_2 = 'google/flan-t5-base'
+
+# Load tokenizer of model
+def load_tokenizer(model_name):
+    return AutoTokenizer.from_pretrained(model_name)
+
+# Load model from model_name
+def load_model(model_name):
+    return AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+model_1 = load_model(model_name_1)
+tokenizer_1 = load_tokenizer(model_name_1)
+model_2 = load_model(model_name_2)
+tokenizer_2 = load_tokenizer(model_name_2)
--- chatbox/models.py
+++ chatbox/models.py
@@ -1,20 +1,5 @@
 from django.db import models
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-# Load tokenizer of model
-def load_tokenizer(model_name):
-    return AutoTokenizer.from_pretrained(model_name)
-
-# Load model from model_name
-def load_model(model_name):
-    return AutoModelForSeq2SeqLM.from_pretrained(model_name)
-
-model_name_1 = 'google/flan-t5-base'
-model_name_2 = 'google/flan-t5-base'
-model_1 = load_model(model_name_1)
-tokenizer_1 = load_tokenizer(model_name_1)
-model_2 = load_model(model_name_2)
-tokenizer_2 = load_tokenizer(model_name_2)
+from .config import model_1, tokenizer_1, model_2, tokenizer_2
 
 # Generate text from model, tokenizer, and prompt
 def generate(model, tokenizer, input_text):
@@ -27,8 +12,6 @@ def generate(model, tokenizer, input_text):
 def new_output(output):
     last_message = output.split(': ')[-1]
     last_model = output.split(':')[-2].split('\n')[-1]
-    print (last_model)
-    print (last_message)
     new_model = 'Model 2' if last_model == 'Model 1' else 'Model 1'
     model = model_1 if new_model == 'Model 1' else model_2
     tokenizer = tokenizer_1 if new_model == 'Model 1' else tokenizer_2
